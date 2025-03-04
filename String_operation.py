3cc64a24d7e656d3a2ebc52ed36e3a151ca5b664
sadiausman-su <usmanbodla047@gmail.com>
2025-03-04 07:16:06 +0000

    Added string operation program

---
+++ String_operation.py
@@ -0,0 +1,8 @@
+#single qoutation mark
+print('welcome to home')
+#double qoutation mark>>>> work same as single qoutation
+print("welcome to home")
+#triple qoutation mark
+print('''welcome to home
+as well as 
+good for well being''')
